79a8330e836ea6a2e64970bcb4ae8cfaa7a72e26
nichtgedacht <db@nichtgedacht.de>
2024-12-11 21:26:48 +0100

    replaced UF2 files

--- src/main.cpp
+++ src/main.cpp
@@ -181,53 +181,6 @@ UBXFrameState state = WaitStart1;
 NavPvt_t NavPvt;
 NavPosllh_t NavPosllh;
 
-void startWatchdog(void) {
-
-    static bool initialized;
-
-    if ( !initialized ) {
-
-        REG_GCLK_GENDIV = GCLK_GENDIV_DIV (1) |             // Use the undivided 32.768kHz clock source
-                          GCLK_GENDIV_ID (2);               // Select Generic Clock (GCLK) 2
-        while ( GCLK->STATUS.bit.SYNCBUSY );                // Wait for synchronization
-
-        REG_GCLK_GENCTRL = GCLK_GENCTRL_GENEN |             // Enable GCLK2
-                           GCLK_GENCTRL_SRC_OSCULP32K |     // Set the clock source to the ultra low power oscillator (OSCULP32K)
-                           GCLK_GENCTRL_ID (2);             // Select GCLK2
-        while ( GCLK->STATUS.bit.SYNCBUSY );                // Wait for synchronization
-
-        // Feed GCLK2 to WDT (Watchdog Timer)
-        REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |             // Enable GCLK2 to the WDT
-                           GCLK_CLKCTRL_GEN_GCLK2 |         // Select GCLK2
-                           GCLK_CLKCTRL_ID_WDT;             // Feed the GCLK2 to the WDT
-        while ( GCLK->STATUS.bit.SYNCBUSY );                // Wait for synchronization
-
-        // Set up WDT
-        REG_WDT_EWCTRL = WDT_EWCTRL_EWOFFSET_256;           // Set the Early Warning Interrupt Time Offset to 7.8125 ms
-        REG_WDT_INTENSET = WDT_INTENSET_EW;                 // Enable the Early Warning interrupt
-        REG_WDT_CONFIG = WDT_CONFIG_PER_1K;                 // Set the WDT reset timeout to 31.25 ms
-        REG_WDT_CTRL = WDT_CTRL_ENABLE;                     // Enable the WDT in normal mode
-        while (WDT->STATUS.bit.SYNCBUSY);                   // Await synchronization of registers between clock domains
-
-        // Configure and enable WDT interrupt
-        NVIC_SetPriority(WDT_IRQn, 0);                      // Top priority
-        NVIC_EnableIRQ(WDT_IRQn);
-
-        initialized = true;
-
-    } else {
-
-       REG_WDT_CTRL = WDT_CTRL_ENABLE;
-       while(WDT->STATUS.bit.SYNCBUSY);
- 
-    }
-}
-
-void stopWatchdog(void) {
-    REG_WDT_CTRL = 0x00;
-    while(WDT->STATUS.bit.SYNCBUSY);
-}
-
 uint16_t distanceHome(double lonRemote, double latRemote ) {
 
     double dx = 1113000 * cos( ( latHome * PI ) / 180 ) * ( lonHome - lonRemote); 
@@ -446,6 +399,53 @@ void parse_UBX_NAV(char c) {
 // -------------------- /UBX ---------------------------
 #endif
 
+void startWatchdog(void) {
+
+    static bool initialized;
+
+    if ( !initialized ) {
+
+        REG_GCLK_GENDIV = GCLK_GENDIV_DIV (1) |             // Use the undivided 32.768kHz clock source
+                          GCLK_GENDIV_ID (2);               // Select Generic Clock (GCLK) 2
+        while ( GCLK->STATUS.bit.SYNCBUSY );                // Wait for synchronization
+
+        REG_GCLK_GENCTRL = GCLK_GENCTRL_GENEN |             // Enable GCLK2
+                           GCLK_GENCTRL_SRC_OSCULP32K |     // Set the clock source to the ultra low power oscillator (OSCULP32K)
+                           GCLK_GENCTRL_ID (2);             // Select GCLK2
+        while ( GCLK->STATUS.bit.SYNCBUSY );                // Wait for synchronization
+
+        // Feed GCLK2 to WDT (Watchdog Timer)
+        REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |             // Enable GCLK2 to the WDT
+                           GCLK_CLKCTRL_GEN_GCLK2 |         // Select GCLK2
+                           GCLK_CLKCTRL_ID_WDT;             // Feed the GCLK2 to the WDT
+        while ( GCLK->STATUS.bit.SYNCBUSY );                // Wait for synchronization
+
+        // Set up WDT
+        REG_WDT_EWCTRL = WDT_EWCTRL_EWOFFSET_256;           // Set the Early Warning Interrupt Time Offset to 7.8125 ms
+        REG_WDT_INTENSET = WDT_INTENSET_EW;                 // Enable the Early Warning interrupt
+        REG_WDT_CONFIG = WDT_CONFIG_PER_1K;                 // Set the WDT reset timeout to 31.25 ms
+        REG_WDT_CTRL = WDT_CTRL_ENABLE;                     // Enable the WDT in normal mode
+        while (WDT->STATUS.bit.SYNCBUSY);                   // Await synchronization of registers between clock domains
+
+        // Configure and enable WDT interrupt
+        NVIC_SetPriority(WDT_IRQn, 0);                      // Top priority
+        NVIC_EnableIRQ(WDT_IRQn);
+
+        initialized = true;
+
+    } else {
+
+       REG_WDT_CTRL = WDT_CTRL_ENABLE;
+       while(WDT->STATUS.bit.SYNCBUSY);
+
+    }
+}
+
+void stopWatchdog(void) {
+    REG_WDT_CTRL = 0x00;
+    while(WDT->STATUS.bit.SYNCBUSY);
+}
+
 // resets referencePressure and set reset flag for GPS
 void altiZero(void) {
     //getSeaLevel() calculates SealevelPressure from realpressure and real altitude MSL
@@ -618,7 +618,9 @@ void setup () {
             delay (500);
         }
 
+#ifdef SERVO
         stopWatchdog();
+#endif
 
         // calc default alfas from ms5611.delta_t for time constants chosen
         // ms5611.delta_t depends on number of sensors and oversampling rates chosen
